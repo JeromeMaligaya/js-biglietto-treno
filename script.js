c48756ec086ea1ccaab81e404c346d84b1b1a20b
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-07-31 18:52:25 +0200

    generazione output

--- script.js
+++ script.js
@@ -3,8 +3,16 @@ console.log('JS OK')
 
 // 1.preparazione dati
     //1.recupero l'elemento dal Dom
-    const resulelement = document.getElementById('price');
-    console.log('id', resulelement);
+    const resultKm = document.getElementById('km');
+    console.log('id', resultKm);
+    const resultAge = document.getElementById('age');
+    console.log('id', resultAge);
+    const resultPrice = document.getElementById('price');
+    console.log('id', resultPrice);
+    const resultDiscount = document.getElementById('discounted');
+    console.log('id', resultDiscount);
+    const resultPriceDiscounted = document.getElementById('price-discounted');
+    console.log('id', resultPriceDiscounted);
 // fine
 
 //2.raccolta dati
@@ -37,14 +45,34 @@ console.log('JS OK')
         console.log('Message Discount', messageDiscount);
     }
     
-    // calcolo prezzo del biglietto scontato   
+    // calcolo prezzo del biglietto scontato 
+    let ticketDiscounted;
+
     if (userAge < 18 || userAge > 65){  
     console.log('discount', discount,'%');
 
-    const ticketDiscounted = (ticketPrice - ((ticketPrice * discount) / 100)).toFixed(2);
+    ticketDiscounted = (ticketPrice - ((ticketPrice * discount) / 100)).toFixed(2);
     console.log('Ticket price with discount €', ticketDiscounted,);
     }
     
 // fine
 
 // 4.generazione output
+// stampo in pagina i km
+resultKm.innerHTML += ` <strong> ${userKm} </strong>`;
+
+// stampo in pagina l'età
+resultAge.innerHTML += ` <strong> ${userAge} </strong>`;
+
+// stampo in pagina il prezzo del biglietto senza sconto
+resultPrice.innerHTML += ` <strong> ${ticketPrice}€ </strong>`;
+
+// stampo in pagina la percentuale dello sconto
+resultDiscount.innerHTML += `<strong> ${discount}% </strong>`;
+
+// stampo in pagina il prezzo finale del biglietto con lo sconto
+if (userAge < 18 || userAge > 65){  
+    resultPriceDiscounted.innerHTML += ` <strong> ${ticketDiscounted}€ </strong>`
+} else {
+    resultPriceDiscounted.innerHTML += ` <strong> ${ticketPrice}€ </strong>`
+}
