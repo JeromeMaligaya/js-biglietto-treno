e0b3b650c95795cea6624dbd9bb2944269048041
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-07-31 15:23:12 +0200

    chiedo all'utente quanti chilometri deve percorrere

--- script.js
+++ script.js
@@ -3,8 +3,16 @@ console.log('JS OK')
 
 // 1.preparazione dati
     //1.recupero l'elemento dal Dom
-    const resulelement = document.getElementById('price')
-    console.log('id', resulelement)
+    const resulelement = document.getElementById('price');
+    console.log('id', resulelement);
+
+    // 2.chiedo all'utente quanti chilometri deve percorrere
+    const userKm = parseInt(prompt('Quanti chilometri devi percorrere?', 150 ));
+    console.log('Km', userKm);
+
+
+
+
 //2.raccolta dati
     
 // 3.lavorazione dati
